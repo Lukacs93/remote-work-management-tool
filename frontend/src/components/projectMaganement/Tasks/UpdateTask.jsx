c10475242f164388eb0c5c037f0a428c4b592681
Balogh Zsolt <101532333+BaloghzsoltCsharp@users.noreply.github.com>
2023-02-15 19:40:45 +0100

    Add a simple skeleton for the UpdateTask component

---
+++ frontend/src/components/projectMaganement/Tasks/UpdateTask.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+const handleSubmit=(e)=>{
+
+
+}
+
+const UpdateTask=()=>
+{
+
+
+
+
+
+
+    return (<div>
+<form onSubmit={handleSubmit}>
+<input id="taskName"></input>
+<input id="assignedWorker"></input>
+<input id="deadLine"></input>
+<input id=""></input>
+<input id=""></input>
+<input id=""></input>
+    <button type="submit">Update</button>
+</form>
+    </div>)
+}
+
+export default UpdateTask
