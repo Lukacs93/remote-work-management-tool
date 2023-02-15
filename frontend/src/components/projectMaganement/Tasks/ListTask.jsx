12a47c553fe934ea7d9148465ed6a7dcb6507581
Balogh Zsolt <101532333+BaloghzsoltCsharp@users.noreply.github.com>
2023-02-15 19:39:51 +0100

    Add basic skeleton for the ListTasks component

---
+++ frontend/src/components/projectMaganement/Tasks/ListTask.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ListTask=(task)=>{
+    return(
+<div>
+    Single Task here
+</div>
+
+
+    )
+}
+
+
+export default ListTask
---
+++ frontend/src/components/projectMaganement/Tasks/ListTasks.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useState } from 'react'
+import {ListTask} from 'ListTask.jsx'
+
+
+const ListTasks=()=>
+{
+    const[loading,IsLoading]=useState(true)
+    const[tasks,setTasks]=useState({},{},{})
+
+
+
+
+    return (<div>
+{loading &&(<div> Loading...</div> )}
+{!loading &&(<div>{tasks.map(task=>(
+
+                    <ListTask task={task}/>
+                    
+                    ))}</div>)}
+    </div>)
+}
+
+export default UpdateTask
