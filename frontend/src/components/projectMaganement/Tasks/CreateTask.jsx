5342e78069cb116ea5d61e74768b49b8931cb709
Balogh Zsolt <101532333+BaloghzsoltCsharp@users.noreply.github.com>
2023-02-15 19:43:36 +0100

    Create a basic skeleton for the CreateTask component

---
+++ frontend/src/components/projectMaganement/Tasks/CreateTask.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+const handleSubmit=(e)=>{
+
+
+}
+
+const CreateTask=()=>
+{
+
+
+
+
+
+
+    return (<div>
+<form onSubmit={handleSubmit}>
+        
+    <input id="taskName"></input>
+    <input id="assignedWorker"></input>
+    <input id="deadLine"></input>
+    <input id=""></input>
+    <input id=""></input>
+    <input id=""></input>
+    <button type="submit">Update</button>
+</form>
+    </div>)
+}
+
+export default CreateTask
