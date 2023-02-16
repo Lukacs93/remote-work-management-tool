1ae4c6d8fc2320d305d98bbad4bd0a6a3f8d997b
Lukacs93 <lukacs.laszlo93@gmail.com>
2023-02-16 01:52:56 +0100

    Add placeholder for slogen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Home />
+        <div className = "head-text">
+          <div className="Slogen-placeholder">
+            <img src="https://indatalabs.com/wp-content/uploads/2020/05/main-video-indatalabs-poster.jpg" style={{width: '100%'}} alt=''/>
+          </div>
+          <div className='text-on-image'>
+            <h1>Remotivate</h1> 
+            <p> Streamline your remote work with Remotivate</p>
+          </div>
+        </div>
       </header>
     </div>
   );
