becc336fe924a1a1e90ce42b0689ff80b6829cff
Lukacs93 <lukacs.laszlo93@gmail.com>
2023-02-16 14:26:35 +0100

    Move slogen placeholder to Header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,13 @@
 import './App.css';
-import Home from './Components/landing-page/Navbar'
+import Navbar from './Components/landing-page/Navbar'
+import Header from './Components/landing-page/Header'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Home />
-        <div className = "head-text">
-          <div className="Slogen-placeholder">
-            <img src="https://indatalabs.com/wp-content/uploads/2020/05/main-video-indatalabs-poster.jpg" style={{width: '100%'}} alt=''/>
-          </div>
-          <div className='text-on-image'>
-            <h1>Remotivate</h1> 
-            <p> Streamline your remote work with Remotivate</p>
-          </div>
-        </div>
+        <Navbar />
+        <Header />
       </header>
     </div>
   );
