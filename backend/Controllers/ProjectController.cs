893a6797b80b1eb149833a42e5d1bc93f5753e8c
Lukacs93 <lukacs.laszlo93@gmail.com>
2023-02-27 23:11:42 +0100

    Create base for project controller

---
+++ backend/Controllers/ProjectController.cs
@@ -0,0 +1,15 @@
+﻿using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+[ApiController, Route("/projects")]
+public class ProjectController : ControllerBase
+{
+    private readonly RemotivateContext _context;
+
+    public ProjectController(RemotivateContext context)
+    {
+        _context = context;
+    }
+}
