e0fecc4c75996833782c8801a1a428abae27a976
Lukacs93 <lukacs.laszlo93@gmail.com>
2023-02-28 00:33:15 +0100

    Implement task model base

---
+++ backend/Models/Entities/Task.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Models.Entities;
+
+public class Task
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; set; }
+}
