e401c52ba4cf63a2351ce506b01b2e73c73d2b7a
Lukacs93 <lukacs.laszlo93@gmail.com>
2023-02-27 23:14:31 +0100

    Create base for task controller

---
+++ backend/Controllers/TaskController.cs
@@ -0,0 +1,15 @@
+﻿using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+[ApiController, Route("/tasks")]
+public class TaskController : ControllerBase
+{
+    private readonly RemotivateContext _context;
+
+    public TaskController(RemotivateContext context)
+    {
+        _context = context;
+    }
+}
