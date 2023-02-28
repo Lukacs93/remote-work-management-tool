e5dcf0f84911a79ed5341353361b180aca67f9d0
Lukacs93 <lukacs.laszlo93@gmail.com>
2023-02-28 01:33:22 +0100

    Implement ProjectService base

---
+++ backend/Services/ProjectService.cs
@@ -0,0 +1,39 @@
+﻿using backend.Data;
+using backend.Models.Entities;
+
+namespace backend.Services;
+
+public class ProjectService : IProjectService
+{
+    private readonly RemotivateContext _context;
+
+    public ProjectService(RemotivateContext context)
+    {
+        _context = context;
+    }
+
+    public Task<IEnumerable<Project>> GetAllProjects()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Project> GetProjectById(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task CreateProject(Project project)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateProject(Project project)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteProject(Project project)
+    {
+        throw new NotImplementedException();
+    }
+}
