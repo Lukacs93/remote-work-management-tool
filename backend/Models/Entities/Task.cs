5dd36249d6391e793845869dc4e1cc05360d412c
Balogh Zsolt <101532333+BaloghzsoltCsharp@users.noreply.github.com>
2023-02-28 01:09:31 +0100

    Create the basic fields for the Task class

--- backend/Models/Entities/Task.cs
+++ backend/Models/Entities/Task.cs
@@ -6,4 +6,13 @@ public class Task
 {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public long Id { get; set; }
+
+    public List<User> UsersOnTask { get; set; }
+
+    public Project Project { get; set; } // The Project its part of
+
+    public string Description { get; set; }
+
+    public Date Date { get; set; }
+
 }
