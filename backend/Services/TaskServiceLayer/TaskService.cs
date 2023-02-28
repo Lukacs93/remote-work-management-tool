d3d8ced1dc4ddb472cf1b9c6d6a31d381cf3f1ec
Lukacs93 <lukacs.laszlo93@gmail.com>
2023-02-28 01:48:07 +0100

    Implement TaskService base

---
+++ backend/Services/TaskServiceLayer/TaskService.cs
@@ -0,0 +1,38 @@
+﻿using backend.Data;
+
+namespace backend.Services.TaskServiceLayer;
+
+public class TaskService : ITaskService
+{
+    private readonly RemotivateContext _context;
+
+    public TaskService(RemotivateContext context)
+    {
+        _context = context;
+    }
+
+    public Task<IEnumerable<Task>> GetAllTasks()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Task> GetTaskById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task CreateTask(Task task)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateTask(Task task)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteTask(Task task)
+    {
+        throw new NotImplementedException();
+    }
+}
