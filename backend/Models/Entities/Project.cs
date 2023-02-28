94882b9b1687e5e1d07d33c64f16d51f03d9d5b2
Balogh Zsolt <101532333+BaloghzsoltCsharp@users.noreply.github.com>
2023-02-28 01:09:19 +0100

    Create the basic fields for the Project class

--- backend/Models/Entities/Project.cs
+++ backend/Models/Entities/Project.cs
@@ -6,4 +6,15 @@ public class Project
 {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public long Id { get; set; }
+
+    public User Manager { get; set; } // The user of the one in charge of the project
+
+    public List<User> UsersInTheProject { get; set; }
+
+    public List<Task> Tasks { get; set; }
+
+    public Date Date { get; set; }
+
+
+
 }
