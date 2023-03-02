fc1fdd90ce17201699e89d1a93fcee010c7e972b
Balogh Zsolt <101532333+BaloghzsoltCsharp@users.noreply.github.com>
2023-03-02 18:48:14 +0100

    Displays all the projects normally

--- frontend/src/components/project-maganement/projects/Project.jsx
+++ frontend/src/components/project-maganement/projects/Project.jsx
@@ -1,22 +1,45 @@
 import React, { useState } from 'react'
 import ListTasks from '../tasks/TaskList'
+import UserList from './UsersList'
 import './Project.css'
 
 const Project = (props) => {
     const [modal,setModal]=useState(false);
 
 
-    console.log(props.project)
+    
     return(
-        <div className='single-project' >
-            <div className='projects-div'>
-                <div>ProjectID: {props.project.id}</div>
-                <div>DateID: {props.project.dateId}</div>
-                <div>ManagerID: {props.project.managerId}</div>
-                Users in the project:{props.project.usersInTheProject.$values!==null &&( <div>{props.project.usersInTheProject.$values.map(user=>{return (<div>{user.firstName} {user.lastName}</div>)})}</div>)}
-                
-                {props.project.tasks.$values!==null &&(<div> <ListTasks /> </div>)}
-                <div>Project Status: {props.project.projectStatus}</div>
+        <div classNameName='single-project' >
+            <div classNameName='projects-div'>
+            <table className="tg">
+<thead>
+  <tr>
+    <th className="tg-0lax">ProjectID: {props.project.id}</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td className="tg-0lax">DateID: {props.project.dateId}</td>
+  </tr>
+  <tr>
+    <td className="tg-0lax">ManagerID: {props.project.managerId}</td>
+  </tr>
+  {props.project.usersInTheProject.$values!==null &&(
+  <tr>
+    <td className="tg-0lax">Users in the project:
+         <tr>{props.project.usersInTheProject.$values.map(user=>{  return <UserList user={user} />})}</tr>
+         
+    </td>
+  </tr>
+  )}
+  <tr>
+    <td className="tg-0lax">Project Status: {props.project.projectStatus}</td>
+  </tr>
+</tbody>
+<tfoot>
+    <button >Tasks for the Project</button>
+</tfoot>
+</table>
             </div>
         </div>
     )
