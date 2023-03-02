08b59ddb7717387a924fe35dea54d64c0669ccd7
ZozoTZM <torokzmark24@gmail.com>
2023-03-02 11:51:30 +0100

    Add user endpoint

---
+++ backend/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using backend.Models.Entities;
+using backend.Services.TaskServiceLayer;
+using backend.Services.UserServiceLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController, Route("/users")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<List<User>> GetAllUsers()
+        {
+            return await _userService.GetAllUsers();
+        }
+
+        [HttpGet("{id:long}")]
+        public async Task<User?> GetUserById(long id)
+        {
+            return await _userService.GetUserById(id);
+        }
+
+        [HttpPost("add-user")]
+        public async Task<User> CreateUser(long userId, [FromBody] User user)
+        {
+            return await _userService.CreateUser(userId, user);
+        }
+
+        [HttpPut]
+        public async Task<User> UpdateUser(User user)
+        {
+            return await _userService.UpdateUser(user);
+        }
+
+        [HttpDelete("{id:long}")]
+        public async Task<User> DeleteUser(long id)
+        {
+            return await _userService.DeleteUser(id);
+        }
+    }
+}
