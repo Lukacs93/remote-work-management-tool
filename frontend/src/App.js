fc338d1a8b3f60e941118ec6371d0e2c72e935ac
Lukacs93 <lukacs.laszlo93@gmail.com>
2023-03-03 02:29:49 +0100

    Add project to navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/landing-page/Header'
 import TaskList from "./components/project-maganement/tasks/TaskList";
 import Dashboard from "./components/dashboard/Dashboard";
 import LandingPage from "./components/landing-page/LandingPage";
+import ProjectDashboard from "./components/project-maganement/ProjectDashboard";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Routes>
             <Route path="/" element={[<Navbar/>,<Header/>, <LandingPage />]} />
             <Route path="/dashboard" element={<Dashboard />} />
-            {/*<Route path="/projects" element={[<Dashboard/>, <TaskList/>]} />*/}
+            <Route path="/projects" element={[<Dashboard/>, <ProjectDashboard/>]} />
             <Route path="/tasks" element={[<Dashboard/>, <TaskList/>]} />
         </Routes>
     </div>
