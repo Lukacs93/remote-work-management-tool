52ad6b73a8c8b193a4b7295edf6a3c229249b277
Lukacs93 <lukacs.laszlo93@gmail.com>
2023-03-15 03:29:06 +0100

    Create RefreshTokenValidator service

---
+++ backend/Services/TokenValidators/RefreshTokenValidator.cs
@@ -0,0 +1,18 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using backend.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace backend.Services.TokenValidators;
+
+public class RefreshTokenValidator
+{
+    private readonly AuthenticationConfiguration _configuration;
+
+    public RefreshTokenValidator(AuthenticationConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+   
+}
