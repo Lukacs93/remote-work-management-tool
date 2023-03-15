63376bb0d0ec40bc44baa51c28aff83dc9ff108c
Lukacs93 <lukacs.laszlo93@gmail.com>
2023-03-16 00:53:26 +0100

    Create login component frontend

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,1 +1,33 @@
-﻿
+﻿import React, { useState } from "react";
+
+function Login() {
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                UserName:
+                <input
+                    type="userName"
+                    value={userName}
+                    onChange={(event) => setUserName(event.target.value)}
+                />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                />
+            </label>
+            <br />
+            <button type="submit">Login</button>
+        </form>
+    );
+}
+
+export default Login;
