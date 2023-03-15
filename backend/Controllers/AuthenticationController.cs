bc3641dcd80ec44fc69c44895b74a72044938a88
Lukacs93 <lukacs.laszlo93@gmail.com>
2023-03-15 02:24:34 +0100

    Add authentication validation to user details

--- backend/Controllers/AuthenticationController.cs
+++ backend/Controllers/AuthenticationController.cs
@@ -1,4 +1,5 @@
-﻿using backend.Models.Requests;
+﻿using backend.Models.Entities;
+using backend.Models.Requests;
 using backend.Services.UserServiceLayer;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,6 +23,29 @@ public class AuthenticationController : Controller
             return BadRequest(new ErrorResponse("Password does not match confirm password"));
         }
     
+        var existingUserByEmail = await _userService.GetByEmail(registerRequest.Email);
+        if (existingUserByEmail != null)
+        {
+            return Conflict(new ErrorResponse("Email already exist"));
+        }
+        
+        var existingUserByUsername = await _userService.GetByUsername(registerRequest.Username);
+        if (existingUserByUsername != null)
+        {
+            return Conflict(new ErrorResponse("Username already exist"));
+        }
+
+        //Create user and store it
+        string passwordHash = _passwordHasher.HashPassword(registerRequest.password);
+        var registrationUser = new TestUser()
+        {
+            Email = registerRequest.Email,
+            Username = registerRequest.Username,
+            PasswordHash = passwordHash
+        };
+
+        await _userService.Create(registrationUser);
+        return Ok();
     }
 
 }
