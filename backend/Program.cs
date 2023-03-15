19861e85e98777eadae7c343b133473274ed290a
Lukacs93 <lukacs.laszlo93@gmail.com>
2023-03-15 03:00:51 +0100

    Add services to configuration

--- backend/Program.cs
+++ backend/Program.cs
@@ -4,6 +4,7 @@ using backend.Data;
 using backend.Services.PasswordHashers;
 using backend.Services.ProjectServiceLayer;
 using backend.Services.TaskServiceLayer;
+using backend.Services.TokenGenerators;
 using backend.Services.UserServiceLayer;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
@@ -18,9 +19,9 @@ builder.Services.AddDbContext<RemotivateContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(b =>
@@ -37,15 +38,17 @@ var configuration = builder.Configuration;
 var authenticationConfiguration = new AuthenticationConfiguration();
 configuration.Bind("Authentication", authenticationConfiguration);
 
+//register
 builder.Services.AddSingleton(authenticationConfiguration);
 
 builder.Services.AddSingleton<AccessTokenGenerator>();
+builder.Services.AddSingleton<RefreshTokenGenerator>();
+builder.Services.AddSingleton<RefreshTokenValidator>();
+builder.Services.AddSingleton<Authenticator>();
+builder.Services.AddSingleton<TokenGenerator>();
 builder.Services.AddSingleton<IPasswordHasher, BcryptPasswordHasher>();
 builder.Services.AddSingleton<IUserService, UserService>();
-
-builder.Services.AddScoped<ITaskService, TaskService>();
-builder.Services.AddScoped<IProjectService, ProjectService>();
-builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddSingleton<IRefreshTokenService, InMemoryRefreshTokenService>();
 
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(o =>
@@ -58,7 +61,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
         ValidAudience = authenticationConfiguration.Audience,
         ValidateIssuerSigningKey = true,
         ValidateIssuer = true,
-        ValidateAudience = true
+        ValidateAudience = true,
+        ClockSkew = TimeSpan.Zero
     };
 });
 
