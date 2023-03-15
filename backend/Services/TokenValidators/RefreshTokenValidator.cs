e467ecf88f94cb4d6d64cead588ac4e6a7ab7189
Lukacs93 <lukacs.laszlo93@gmail.com>
2023-03-15 03:30:13 +0100

    Implement validate method to validate refresh token

--- backend/Services/TokenValidators/RefreshTokenValidator.cs
+++ backend/Services/TokenValidators/RefreshTokenValidator.cs
@@ -14,5 +14,30 @@ public class RefreshTokenValidator
         _configuration = configuration;
     }
 
-   
+    public bool Validate(string refreshToken)
+    {
+        var validationParameters =  new TokenValidationParameters()
+        {
+            IssuerSigningKey =
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.RefreshTokenSecret)),
+            ValidIssuer = _configuration.Issuer,
+            ValidAudience = _configuration.Audience,
+            ValidateIssuerSigningKey = true,
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ClockSkew = TimeSpan.Zero
+        };
+        
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        try
+        {
+            tokenHandler.ValidateToken(refreshToken, validationParameters, out SecurityToken validatedToken);
+            return true;
+        }
+        catch (Exception exception)
+        {
+            return false;
+        }
+    }
 }
