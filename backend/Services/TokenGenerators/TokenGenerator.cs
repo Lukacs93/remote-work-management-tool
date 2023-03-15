2e0aec7f56f84d5e95b2e627123c74ce56d0e693
Lukacs93 <lukacs.laszlo93@gmail.com>
2023-03-15 03:20:57 +0100

    Move generate token to TokenGenerator service

---
+++ backend/Services/TokenGenerators/TokenGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace backend.Services.TokenGenerators;
+
+public class TokenGenerator
+{
+    public string GenerateToken(string secretKey, string issuer, string audience, 
+        double expirationMinutes, IEnumerable<Claim> claims = null)
+    {
+        SecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer,
+            audience,
+            claims,
+            DateTime.UtcNow,
+            DateTime.UtcNow.AddMinutes(expirationMinutes),
+            credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
