347bb76ec72f97d7fee8610f8f0f27cae1247673
Lukacs93 <lukacs.laszlo93@gmail.com>
2023-03-15 02:01:49 +0100

    Add RegisterRequest model to backend

---
+++ backend/Controllers/AuthenticationController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+public class AuthenticationController : Controller
+{
+    
+}
---
+++ backend/Models/Requests/RegisterRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models.Requests;
+
+public class RegisterRequest
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Required]
+    public string Username { get; set; }
+    [Required]
+    // [MinLength(5)]
+    public string password { get; set; }
+    [Required]
+    public string ConfirmPassword { get; set; }
+}
