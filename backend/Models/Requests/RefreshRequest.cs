24f6d729eb9a307a4f1fa237c05c2fabe0fb8727
Lukacs93 <lukacs.laszlo93@gmail.com>
2023-03-15 03:26:26 +0100

    Create RefreshRequest model

---
+++ backend/Models/Requests/RefreshRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models.Requests;
+
+public class RefreshRequest
+{
+    [Required]
+    public string RefreshToken { get; set; }
+}
