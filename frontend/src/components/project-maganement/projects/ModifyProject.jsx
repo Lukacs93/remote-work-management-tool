777f37f752e5a47f79638fc1fbd9a2eda51b53ba
Balogh Zsolt <101532333+BaloghzsoltCsharp@users.noreply.github.com>
2023-03-16 20:49:33 +0100

    Modify the modifyProject component to better fit the backend

--- frontend/src/components/project-maganement/projects/ModifyProject.jsx
+++ frontend/src/components/project-maganement/projects/ModifyProject.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import {Link, useParams} from "react-router-dom";
 import './ModifyProject.css'
 
 const UpdateProject = (props) =>
@@ -7,17 +8,43 @@ const UpdateProject = (props) =>
     const [form,setForm] = useState(
         {
         "id": props.project.id,
-        "DateId": 0,
-        "ManagerID": 0,
+        "DeadLine": "",
         "ProjectStatus": 0,
-        "usersInTheProject": props.project.usersInTheProject,
+        "Finished":false,
         "tasks": props.project.tasks
     })
+
+    const isValidDate=(dateString)=>{
+        // First check for the pattern
+        if(!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString))
+            return false;
+    // Parse the date parts to integers
+    var parts = dateString.split("/");
+    var day = parseInt(parts[1], 10);
+    var month = parseInt(parts[0], 10);
+    var year = parseInt(parts[2], 10);
     
+    // Check the ranges of month and year
+    if(year < 1000 || year > 3000 || month == 0 || month > 12)
+        return false;
+    
+    var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+    
+    // Adjust for leap years
+    if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+        monthLength[1] = 29;
+    
+    // Check the range of the day
+    return day > 0 && day <= monthLength[month - 1];
+    }
+
+    const isFinished=()=>{
+
+        setForm({...form, Finished: !form.Finished})
+    }
     const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log(form)
-        console.log(props.project)
+if(isValidDate(form.DeadLine))
         await fetch(`https://localhost:7029/projects/${props.project.id}`,{
             method: 'PUT',
             headers: {
@@ -42,22 +69,25 @@ const UpdateProject = (props) =>
             <div className="update-project-form-container">
                 {showSuccessText ?
                     <p className="project-delete-message">Project Successfully Updated</p>
-                    :                    
+                    :
                     <form className="update-project-form" onSubmit={handleSubmit}>
                         <h2>Edit Project</h2>
-                        <input className="update-project-input" id="DateID" 
-                             placeholder='DateID'
-                             onChange={(e) => setForm({...form, DateId: parseInt(e.target.value)})}
+                        <label>DeadLine
+                        <input className="update-project-input" id="Date" 
+                             placeholder='DD/MM/YYY'
+                             onChange={(e) => setForm({...form, DeadLine: parseInt(e.target.value)})}
                          />
-                        <input className="update-project-input" id="ManagerID"
-                              placeholder='ManagerID'
-                              onChange={(e) => setForm({...form, ManagerID: parseInt(e.target.value)})}
-                        />
+                         </label>
                         <input className="update-project-input" id="ProjectStatus"
                               placeholder='Project Status'
                               onChange={(e) => setForm({...form, ProjectStatus: parseInt(e.target.value)})}
                         />
-                        <button className="update-project-submit-button" type="submit">Save</button>
+                        {form.Finished && 
+                        <button className="update-project-finished-button" onClick={isFinished}>Finished</button>}
+                        {!form.Finished && 
+                        <button className="update-project-finished-button" onClick={isFinished}>Not Finished</button>}
+
+                        <button className="update-project-submit-button"  type="submit">Save</button>
                     </form>
                 }
             </div>
