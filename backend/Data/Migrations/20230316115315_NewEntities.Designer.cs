bf69a1960f03094e94af2f6be17ab79da54ad235
Balogh Zsolt <101532333+BaloghzsoltCsharp@users.noreply.github.com>
2023-03-16 12:56:44 +0100

    Modifie migration

--- backend/Data/Migrations/20230316115315_NewEntities.Designer.cs
+++ backend/Data/Migrations/20230316115315_NewEntities.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using backend.Data;
 
@@ -11,9 +12,11 @@ using backend.Data;
 namespace backend.Data.Migrations
 {
     [DbContext(typeof(RemotivateContext))]
-    partial class RemotivateContextModelSnapshot : ModelSnapshot
+    [Migration("20230316115315_NewEntities")]
+    partial class NewEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
