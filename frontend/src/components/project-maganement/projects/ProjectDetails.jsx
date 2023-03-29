e3a6d56c16ab0b4ee0dbef9753895f30ab91156f
Balogh Zsolt <101532333+BaloghzsoltCsharp@users.noreply.github.com>
2023-03-29 10:28:57 +0200

    Add correct project details for showing to the user

--- frontend/src/components/project-maganement/projects/ProjectDetails.jsx
+++ frontend/src/components/project-maganement/projects/ProjectDetails.jsx
@@ -1,31 +1,65 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react'
 import UserList from './UsersList';
+import Date from '../date/Date'
+const ProjectDetails = (props) =>{
+    const [dates,setDates]=useState()
+    const [manager, setManager]=useState({
+        "firstName":"",
+        "lastName":""
+    })
+    useEffect(()=>
+    {
+        const getUser=async()=>{
+            await fetch(`https://localhost:7029/users/${props.project.managerId}`)
+                .then((resp)=>resp.json())
+                .then((resp)=>{setManager(resp)
+                    setTimeout(() => {
+                    }, 3000);
+                })
+        }
+        const getDates=async()=>{
+            await fetch(`https://localhost:7029/dates/${props.project.dateId}`)
+                .then((resp)=>resp.json())
+                .then((resp)=>{setDates(resp)
+                    setTimeout(() => {
+                    }, 3000);
+                })
+        }
+        getDates()
+        getUser()
 
-const ProjectDetails = (project) =>{
-    console.log(project.project);
-
+    },[ ])
     return(
         <>
-        <div className="project-details-content animate">
-            <div className="project-details-container"> 
-                <div className="project-description">
-                    <h2>Project Details</h2>
-                    <p>{/*project.projectItem.details*/}</p>
-                </div>
-                <div className="project-id">ProjectID: {project.project.id}</div>
-                <div className="date-id">DateID: {project.project.dateId}</div>
-                <div className="manager-id">ManagerID: {project.project.managerId}</div>
-                {project.project.usersInTheProject !== null && (
-                    <div className="users">
-                        <div className="users-title">Users in the project:</div>
-                        {project.project.usersInTheProject.map(user=>{
-                            return <UserList user={user}/>
-                        })}
-                    </div>
-                )}
-                <div className="project-status">Project Status: {project.project.projectStatus}</div>
-            </div>
-        </div>
+        <div className="project-details-container">
+                                    <div className="project-description">
+                                        <h1 className='projectDetailsHeader'>Details</h1>
+                                        <p className="projectDescriptionParagraph">{props.project.description}
+                                        </p>
+                                    </div>
+                                    <div className="project-details">
+                                        <div className="project-id">Title: {props.project.name}</div>
+                                        <div className="manager"> Manager: {manager.firstName} {manager.lastName}</div>
+                        {dates!==undefined && <Date date={dates}/>}
+                                        {props.project.usersOnProject !== null && (
+                                            <div className="users">
+                                                <div className="users-title">Users in the project: {props.project.usersOnProject.map(user => {
+                                                    return <UserList user={user}/>
+                                                })}</div>
+
+                                            </div>
+                                        )}
+                                        <div className="project-status">Project Status:
+                                        {
+                                            props.project.projectStatus === 1 ? " In Progress"
+                                                : props.project.projectStatus === 2 ? " Review"
+                                                    : props.project.projectStatus === 3 ? " Done" 
+                                                        : " Not Started"
+                                        }
+                                        
+                                        </div>
+                                    </div>
+                                </div>
         </>
     )
 }
