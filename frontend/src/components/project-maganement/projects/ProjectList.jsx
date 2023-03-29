9a475c5d9adf21e24ef5ab799fcda1723dc48f3b
Balogh Zsolt <101532333+BaloghzsoltCsharp@users.noreply.github.com>
2023-03-29 10:29:14 +0200

    Remove deadcodes

--- frontend/src/components/project-maganement/projects/ProjectList.jsx
+++ frontend/src/components/project-maganement/projects/ProjectList.jsx
@@ -13,33 +13,6 @@ const ProjectList = (props) =>
             "lastName":""
         })
 
-    // useEffect(()=>
-    // {
-    //     async function getProjects() {
-    //         const response = await fetch(`https://localhost:7029/projects`, {
-    //             method: "GET",
-    //             headers: {
-    //                 "Authorization": `Bearer ${localStorage.getItem("token")}`
-    //             }
-    //         });
-    //
-    //         if (!response.ok) {
-    //             const message = `An error occurred: ${response.statusText}`;
-    //             alert(message);
-    //             return;
-    //         }
-    //
-    //         const result = await response.json();
-    //         setProjects(result)
-    //         setTimeout(() => {
-    //             setIsLoading("done")
-    //         }, 1000);
-    //     }
-    //
-    //     getProjects();
-    //
-    // },[props.isSubmit, isModified])
-
     const jwtToken = localStorage.getItem("token");
     const userID = jwt_decode(jwtToken).id;
     
@@ -53,15 +26,7 @@ const ProjectList = (props) =>
                     }, 3000);
                 })
         }
-        // const getProjects = async() => { 
-        //     await fetch(`https://localhost:7029/projects`)
-        //         .then((resp)=>resp.json())
-        //         .then((resp)=>{setProjects(resp)
-        //             setTimeout(() => {
-        //             setIsLoading("done")
-        //             }, 1000);
-        //         })
-        // }
+
         async function getProjects() {
             const response = await fetch(`https://localhost:7029/projects`, {
                 method: "GET",
