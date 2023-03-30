682d894f42c54752869446308bbfaef02a7032b8
ZozoTZM <torokzmark24@gmail.com>
2023-03-30 08:52:36 +0200

    Add route for statistics to App.js

---
+++ frontend/src/components/statistics/ReportsPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+const Reports = () =>{
+
+
+
+
+    return(
+        <>
+        <div>
+            
+        </div>   
+        </>
+    )
+}
+export default Reports;
