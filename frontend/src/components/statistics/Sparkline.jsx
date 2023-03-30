805ad34cc88b01ef64c1bc2e7d3eebd561fa6a74
ZozoTZM <torokzmark24@gmail.com>
2023-03-30 10:25:02 +0200

    Create sparkline chart

---
+++ frontend/src/components/statistics/Sparkline.jsx
@@ -0,0 +1,14 @@
+import {
+    Sparkline,
+    ChartValueAxis,
+    ChartValueAxisItem,
+  } from "@progress/kendo-react-charts";
+  import { COLORS } from "./constants";
+
+  const SparklineContainer = () => (
+    <div>
+      
+    </div>
+  );
+  
+  export default SparklineContainer;
