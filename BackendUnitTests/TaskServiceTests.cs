7c7a66e340a9fe5caa75bf7c009497110ffe87d6
Lukacs93 <lukacs.laszlo93@gmail.com>
2023-03-30 19:17:24 +0200

    Implement Factory Pattern to simplify test data creation

--- BackendUnitTests/TaskServiceTests.cs
+++ BackendUnitTests/TaskServiceTests.cs
@@ -1,25 +1,11 @@
 using backend.Data;
-using backend.Models.Entities;
 using backend.Services.TaskServiceLayer;
 using Microsoft.EntityFrameworkCore;
-using Moq;
-
 
 namespace BackendUnitTests;
 
-
 public class TaskServiceTests
 {
-    private Mock<RemotivateContext> _contextMock;
-    private TaskService _taskService;
-
-    [SetUp]
-    public void Setup()
-    {
-        _contextMock = new Mock<RemotivateContext>(new DbContextOptions<RemotivateContext>());
-        _taskService = new TaskService(_contextMock.Object);
-    }
-
     [Test]
     public async Task GetAllTasks_ReturnsTasks()
     {
@@ -30,12 +16,7 @@ public class TaskServiceTests
 
         using (var context = new RemotivateContext(options))
         {
-            var tasks = new List<TaskItem>
-            {
-                new TaskItem { Id = 1, Description = "Task description1", Name = "Task 1", ProjectId = 1, UsersOnTask = new List<User>() },
-                new TaskItem { Id = 2, Description = "Task description2", Name = "Task 2", ProjectId = 2, UsersOnTask = new List<User>() },
-                new TaskItem { Id = 3, Description = "Task description3", Name = "Task 3", ProjectId = 3, UsersOnTask = new List<User>() }
-            };
+            var tasks = TestDataFactory.GetTestTasks();
 
             context.Tasks.AddRange(tasks);
             context.SaveChanges();
@@ -55,7 +36,6 @@ public class TaskServiceTests
             Assert.AreEqual("Task description3", result[2].Description);
         }
     }
-    
     [Test]
     public async Task GetTaskById_ReturnsTask()
     {
@@ -66,7 +46,7 @@ public class TaskServiceTests
 
         using (var context = new RemotivateContext(options))
         {
-            var task = new TaskItem { Id = 1, Description = "Task description1", Name = "Task 1", ProjectId = 1, UsersOnTask = new List<User>() };
+            var task = TestDataFactory.GetTestTask(1);
 
             context.Tasks.Add(task);
             context.SaveChanges();
@@ -80,30 +60,18 @@ public class TaskServiceTests
             var result = await taskService.GetTaskById(1);
 
             // Assert
+            Assert.IsNotNull(result);
             Assert.AreEqual(1, result.Id);
             Assert.AreEqual("Task description1", result.Description);
-            Assert.AreEqual("Task 1", result.Name);
-            Assert.AreEqual(1, result.ProjectId);
-            Assert.IsEmpty(result.UsersOnTask);
         }
     }
-    
+
     [Test]
     public async Task GetUsersByTaskId_ReturnsUsers()
     {
         // Arrange
         var taskId = 1;
-        var task = new TaskItem
-        {
-            Id = taskId,
-            Name = "TaskName",
-            Description = "Task 1",
-            UsersOnTask = new List<User>
-            {
-                new User { Id = 1, Email = "test@test.com", Role = "User", Username = "User 1", FirstName = "TestFirstName", LastName = "TestLAstName", PasswordHash = "hashedPass"},
-                new User { Id = 2, Email = "test@test.com", Role = "User", Username = "User 2", FirstName = "TestFirstName", LastName = "TestLAstName", PasswordHash = "hashedPass"}
-            }
-        };
+        var task = TestDataFactory.GetTestTaskWithUsers(taskId, TestDataFactory.GetTestUsers());
 
         var options = new DbContextOptionsBuilder<RemotivateContext>()
             .UseInMemoryDatabase(databaseName: "GetUsersByTaskId_ReturnsUsers")
---
+++ BackendUnitTests/TestDataFactory.cs
@@ -0,0 +1,35 @@
+﻿using backend.Models.Entities;
+
+namespace BackendUnitTests;
+
+public static class TestDataFactory
+{
+    public static List<TaskItem> GetTestTasks()
+    {
+        return new List<TaskItem>
+        {
+            new TaskItem { Id = 1, Description = "Task description1", Name = "Task 1", ProjectId = 1, UsersOnTask = new List<User>() },
+            new TaskItem { Id = 2, Description = "Task description2", Name = "Task 2", ProjectId = 2, UsersOnTask = new List<User>() },
+            new TaskItem { Id = 3, Description = "Task description3", Name = "Task 3", ProjectId = 3, UsersOnTask = new List<User>() }
+        };
+    }
+
+    public static TaskItem GetTestTask(int id)
+    {
+        return new TaskItem { Id = id, Description = $"Task description{id}", Name = $"Task {id}", ProjectId = id, UsersOnTask = new List<User>() };
+    }
+
+    public static TaskItem GetTestTaskWithUsers(int id, List<User> users)
+    {
+        return new TaskItem { Id = id, Description = $"Task description{id}", Name = $"Task {id}", ProjectId = id, UsersOnTask = users };
+    }
+
+    public static List<User> GetTestUsers()
+    {
+        return new List<User>
+        {
+            new User { Id = 1, Email = "test@test.com", Role = "User", Username = "User 1", FirstName = "TestFirstName", LastName = "TestLAstName", PasswordHash = "hashedPass"},
+            new User { Id = 2, Email = "test@test.com", Role = "User", Username = "User 2", FirstName = "TestFirstName", LastName = "TestLAstName", PasswordHash = "hashedPass"}
+        };
+    }
+}
